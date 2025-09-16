76460039b48fc9c01814251b042bb6588d54b63e
Yamika1 <161420780+Yamika1@users.noreply.github.com>
2025-09-16 15:17:25 +0200

    Add project files.

---
+++ Prog2Bpartone/Controllers/MakePayments.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prog2Bpartone.Controllers
+{
+    public class MakePayments : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
---
+++ Prog2Bpartone/Controllers/SubmitClaim.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Prog2Bpartone.Controllers
+{
+    public class SubmitClaim : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
